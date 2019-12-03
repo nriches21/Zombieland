3f2259069e4133794e577662c472e1fc0ed5fdcd
Nora L Riches <nriches21@my.whitworth.edu>
2019-12-02 18:07:42 -0800

    made other changeS

--- denizen.h
+++ denizen.h
@@ -10,60 +10,48 @@ using std::string;
 
 
 class Denizen {
-<<<<<<< Updated upstream
-private:
-	string name;
-=======
 protected:
-	string name; 
+	string name;
 	string status; //ignorant / alarmed / zombie
 	bool turnOver; //Already did an action this turn- if bit this turn, cannot also move and vice versa. Has to be reset after every time tick
 	float biteChance; //probability of being bitten
 
->>>>>>> Stashed changes
 public:
 	//constructor for denizen that just initializes name
 	Denizen(string name) : name(name) { turnOver = false; }
 
 	//getters for the variables
 	string getName() { return name; }
-<<<<<<< Updated upstream
-	void speak() { cout << "Hello, world!! My name is " << name << endl; }
-=======
 	string getStatus() { return status; }
-	float getBiteChance() { return biteChance;  }
+	float getBiteChance() { return biteChance; }
 
 	//testing method
 	virtual void speak() = 0;
-	
->>>>>>> Stashed changes
+
+	//overload of the < 
+	Denizen operator < (Denizen const& i1, Denizen const& i2) {
+		if (i1.getBiteChance() <= i2.getBiteChance()) {
+			return i1;
+		}
+		else {
+			return i2;
+		}
+	}
+
 };
 
+
 class Zombie : public Denizen {
-private:
-	string status;
 public:
-<<<<<<< Updated upstream
-	Zombie(string name) : Denizen (name), status("Zombie") {}
-	string getStatus() { return status; }
-=======
 	//Zombie constructor
 	Zombie(string name) : Denizen(name) { status = "Zombie"; biteChance = 0; }
 
 	//test method
->>>>>>> Stashed changes
 	void speak() { cout << "mngfdfgjfdfghdf brains......" << endl; }
 };
 
 class Alarmed : public Denizen {
-private:
-	string status;
 public:
-<<<<<<< Updated upstream
-	Alarmed(string name) : Denizen(name), status("Alarmed") {}
-	string getStatus() { return status; }
-	void speak() { cout << "AAAAAAAAAAAAAAAAAAAAAAAAAAA!!!" << endl; }
-=======
 	Alarmed(string name) : Denizen(name) { status = "Alarmed"; }
 
 	//test method
@@ -80,7 +68,6 @@ public:
 
 	//test method
 	void speak() { cout << "My word- what a fine, normal day in Simville!" << endl; }
->>>>>>> Stashed changes
 };
 
 #endif
--- district.h
+++ district.h
@@ -1,41 +1,81 @@
 #ifndef DISTRICT_H_
 #define DISTRICT_H_
 
+#include <queue>
+#include <iostream>
+
 class District {
 private:
 	string name;
+	list<Denizen*> populace;
+	map<char, District*> connections;
+	int zombie;
+	int alarmed;
+	int ignorant;
+
 public:
 	District(string name) : name(name) {}
-	string getName() const{ return name; }
-<<<<<<< Updated upstream
-=======
+
+	string getName() const { return name; }
 
 	int zombieTotal() { return zombies; }
 	int alarmedTotal() { return alarmed; }
 	int ignorantTotal() { return ignorant; }
 
-	void addDenizen(Denizen* de) { populace.push_back(de);	}
+	void addDenizen(Denizen* de) { populace.push_back(de); }
 
-	//iterate through the population
-	//setBiteChance in here??
-	//	if (alarmed){
-	//		biteChance = 25;
-	//		biteChance += density of district; //need to make sure that the density of the district is somewhere between 0 and 50
-	//	}
-	// if (ignorant){
-	//		bitechance = 45;
-	//		biteChance += density of district; //need to make sure that the density of the district is somewhere between 0 and 50
-	//	}
+	//iterate through the population in the main
+	void setBiteChance(Denizen inputDenizen) {
+		if (inputDenizen.getStatus() == "alarmed") {
+			inputDenizen.biteChance = 25 + density; //set the bite chance to be 25 (base chance for alarmed) and add the density of the district (some number between 0 and 50)
+		}
+		if (inputDenizen.getStatus() == "ignorant") {
+			inputDenizen.biteChance = 45 + density; //set the bite chance to be 45 (base chance for ignorant) and add the density of the district (some number between 0 and 50)
+		}
+	}
+
+	bool compare(const Denizen& first, const Denizen& second) {
+		return (first > second); //return true if first is greater than the last
+		//http://www.cplusplus.com/forum/beginner/91610/ help from this -- use it because idk if this is right
+	}
+
+	void sortPopulace() {
+		populace.sort(compare); //automatically sorts in descending order(using the operator < overload int eh denizen class)
+	}
+
+	void bite() {
+		if (zombie != 0) {
+			queue<Denizen*> biteAttempt; //queue of denizens that are going to be attempted to be bitten by zombies based on bite chance
+		}
+		else {
+			return;
+		}
+		sortPopulace(); //sort populace in descending order by bite chance
+		std::list<iterator> it = populace.begin(); //iterator pointing to the beginning of the list
+		for (int i = zombie; i != 0; i--) { //for loop that runs for the number of zombiest there are
+			while (it != populace.end()) { //as long as we are not at the end of the populace, keep iterating
+				if (*it->getStatus != "zombie") { //check if we are at zombies
+					biteAttempt.push(it*); //if it is not a zombie, push the denizen into the queue to be attempted to be bitten
+				}
+				it++; //increment the iterater
+			}
+		}
+
+		for (int i = zombie; (i != 0) && (!biteAttempt.empty()); i--) { //iterate through all the zombies as long as there are people in the queue
+
+		}
+	}
 
 	//bite()	
 	/**
-	* Each district checks to see if there are zombies, if yes- 
-	* then creates a queue of citazens based on bite chance that is the 
-	* size of the # of zombies which pushes denizens in based on bite chance, 
-	* and a for loop for # of zombies which determines whether bite was successful- 
-	*	If success, 
+	* Each district checks to see if there are zombies, if yes- done
+	* then creates a queue of denizens based on bite chance that is the done
+	* size of the # of zombies which pushes denizens in based on bite chance, done
+	* and a for loop for # of zombies which determines whether bite was successful-
+	*	If success,
 	* Another person is pushed into the queue if that person in the queue moves to another district
-	* Map or list of denizens sorted by bite chance 
+	* Map or list of denizens sorted by bite chance
+	* change turnOver status
 	**/
 
 	//alarmedTellIgnorant()	
@@ -43,7 +83,8 @@ public:
 	* Each district checks to see if there are alarmed, if yes-
 	* for loop the size of numalarmed
 	* iterate through list of people
-	* if ignorant, delete them and create a new alarmed 
+	* if ignorant, delete them and create a new alarmed
+	* change their turnOver status once they have been created
 	**/
 
 	//move()	
@@ -51,10 +92,10 @@ public:
 	* Iterate through the entire list of denizens, check if denizen already made an action (turnOver bool)
 	* If not, then check if ignorant; if ignorant, check time tick, and if they move this tick, then move to home or work.
 	* Simville has a map of denizens and districts. move() takes a pointer to Simville's map and modifies denizen locations.
-	* map<denizen*, district name string>some_map 
+	* map<denizen*, district name string>some_map
 	* After move, delete denizen from this list and push new pair to simville's map.
-	* Zombie and Alarmed move- move into any connected district at random. Delete from list and push new pair to map. 
-	* District has to update list based on Simville's map each time tick.	
+	* Zombie and Alarmed move- move into any connected district at random. Delete from list and push new pair to map.
+	* District has to update list based on Simville's map each time tick.
 	**/
 
 	//iterate through entire list of denizens and reset turnOver to bool 
@@ -99,7 +140,6 @@ public:
 		}
 	}
 
->>>>>>> Stashed changes
 };
 
 #endif
